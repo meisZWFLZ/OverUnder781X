a0a44a7175f3730303a043c4fb684ecc0eefea44
meisZWFLZ <atcurtis08@gmail.com>
2023-10-28 15:45:49 -0700

    Fix scoreAllianceTriball

--- src/auton/actions/scoreAllianceTriball.cpp
+++ src/auton/actions/scoreAllianceTriball.cpp
@@ -5,16 +5,15 @@
 
 using namespace fieldDimensions;
 void auton::actions::scoreAllianceTriball() {
-  Robot::chassis->moveTo(/* leftOrRight()* */60/* (MAX_X - TILE_RADIUS) */,-45/* (MIN_Y + ( TILE_LENGTH*1.5 - 9)) */, UP, 5000,false,true,0,0.6);
-  pros::delay(100);
+  Robot::chassis->moveTo(leftOrRight()*(MAX_X - TILE_RADIUS + 1),(MIN_Y + ( TILE_LENGTH*1.5 - 3)), UP, 5000,false,true,0,0.2);
   Robot::Actions::outtake();
-  pros::delay(250);
+  pros::delay(100);
   Robot::chassis->tank(127,127);
   pros::delay(100);
   Robot::Actions::stopIntake();
-  pros::delay(900);
-  Robot::chassis->tank(-32,-32);
-  pros::delay(500);
+  pros::delay(200);
+  Robot::chassis->tank(-127,-172);
+  pros::delay(225);
   Robot::chassis->tank(0,0);
 
 }
