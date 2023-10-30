712b9220ad4590e086f0f8aed19e07a33daa8377
meisZWFLZ <atcurtis08@gmail.com>
2023-10-30 15:04:49 -0700

    Remove todo

--- src/auton/actions/touchElevationBar.cpp
+++ src/auton/actions/touchElevationBar.cpp
@@ -8,7 +8,7 @@ using namespace fieldDimensions;
 void auton::actions::touchElevationBar() {
   Robot::chassis->moveTo(leftOrRight() * (0 + TILE_RADIUS - 4),
                          (MIN_Y + TILE_LENGTH * 1.5 + 0.75),
-                         /* @TODO: FIX  */ leftOrRight() * LEFT + 10, 5000);
+                         leftOrRight() * LEFT + 10, 5000);
   // Robot::chassis->moveTo(/* auton?::leftOrRight()*
   // */0-Robot::Dimensions::drivetrainLength/2, MIN_Y + TILE_RADIUS, RIGHT,
   // 5000);
