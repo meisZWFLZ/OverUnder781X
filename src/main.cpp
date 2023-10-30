89c0755b12ce4ba7e01f485b5ac1be4bc9cffbb9
Andrew Curtis <80860310+meisZWFLZ@users.noreply.github.com>
2023-10-30 20:56:10 +0000

    Remove everything but shooting

--- src/main.cpp
+++ src/main.cpp
@@ -13,56 +13,56 @@
 #include <string.h>
 #include "fieldDimensions.h"
 
-float average(std::vector<float> const& v) {
-  if (v.empty()) { return 0; }
+// float average(std::vector<float> const& v) {
+//   if (v.empty()) { return 0; }
 
-  auto const count = static_cast<float>(v.size());
-  return std::reduce(v.begin(), v.end()) / count;
-}
+//   auto const count = static_cast<float>(v.size());
+//   return std::reduce(v.begin(), v.end()) / count;
+// }
 
-const float get_wattage(pros::Motor_Group& group) {
-  std::vector<float> watts = {};
-  for (int i = 0; i < group.size(); i++) watts.push_back(group[i].get_power());
-  return average(watts);
-}
+// const float get_wattage(pros::Motor_Group& group) {
+//   std::vector<float> watts = {};
+//   for (int i = 0; i < group.size(); i++) watts.push_back(group[i].get_power());
+//   return average(watts);
+// }
 
 void screen() {
-  // loop forever
-  pros::Controller controller(pros::controller_id_e_t::E_CONTROLLER_MASTER);
-  while (true) {
-    lemlib::Pose pose =
-        Robot::chassis->getPose(); // get the current position of the robot
-    pros::lcd::clear_line(0);
-    pros::lcd::print(0, "x: %f in", pose.x); // print the x position
-    pros::lcd::clear_line(1);
-    pros::lcd::print(1, "y: %f in", pose.y); // print the y position
-    pros::lcd::clear_line(2);
-    pros::lcd::print(2, "heading: %f deg",
-                     pose.theta); // print the heading
-    if (!controller.get_digital(
-            pros::controller_digital_e_t::E_CONTROLLER_DIGITAL_UP)) {
-    } else {
-      // display wattage
-      // controller.clear_line(0);
-      printf("in: %fw\n", get_wattage(Robot::Motors::intake));
-      // char intake[15];
-      // sprintf(intake, "in: %fw",
-      //         get_wattage(Robot::Motors::intake)); // print the x position
-      // controller.set_text(0, 0, intake);
-      // // controller.clear_line(1);
-      char shooter[15];
-      sprintf(shooter, "fly: %fw",
-              get_wattage(Robot::Motors::shooter)); // print the x position
-      controller.set_text(0, 0, shooter);
-      // char drive[15];
-      // sprintf(drive, "dri: %fw",
-      //         (get_wattage(Robot::Motors::leftDrive) +
-      //          get_wattage(Robot::Motors::rightDrive)) /
-      //             2); // print the x position
-      // controller.set_text(2, 0, drive); // print the heading
-    }
-    pros::delay(200);
-  }
+  // // loop forever
+  // pros::Controller controller(pros::controller_id_e_t::E_CONTROLLER_MASTER);
+  // while (true) {
+  //   lemlib::Pose pose =
+  //       Robot::chassis->getPose(); // get the current position of the robot
+  //   pros::lcd::clear_line(0);
+  //   pros::lcd::print(0, "x: %f in", pose.x); // print the x position
+  //   pros::lcd::clear_line(1);
+  //   pros::lcd::print(1, "y: %f in", pose.y); // print the y position
+  //   pros::lcd::clear_line(2);
+  //   pros::lcd::print(2, "heading: %f deg",
+  //                    pose.theta); // print the heading
+  //   if (!controller.get_digital(
+  //           pros::controller_digital_e_t::E_CONTROLLER_DIGITAL_UP)) {
+  //   } else {
+  //     // display wattage
+  //     // controller.clear_line(0);
+  //     printf("in: %fw\n", get_wattage(Robot::Motors::intake));
+  //     // char intake[15];
+  //     // sprintf(intake, "in: %fw",
+  //     //         get_wattage(Robot::Motors::intake)); // print the x position
+  //     // controller.set_text(0, 0, intake);
+  //     // // controller.clear_line(1);
+  //     char shooter[15];
+  //     sprintf(shooter, "fly: %fw",
+  //             get_wattage(Robot::Motors::shooter)); // print the x position
+  //     controller.set_text(0, 0, shooter);
+  //     // char drive[15];
+  //     // sprintf(drive, "dri: %fw",
+  //     //         (get_wattage(Robot::Motors::leftDrive) +
+  //     //          get_wattage(Robot::Motors::rightDrive)) /
+  //     //             2); // print the x position
+  //     // controller.set_text(2, 0, drive); // print the heading
+  //   }
+  //   pros::delay(200);
+  // }
 }
 
 /**
@@ -72,31 +72,31 @@ void screen() {
  * to keep execution time for this mode under a few seconds.
  */
 void initialize() {
-  Robot::initializeOdometryConfig();
-  printf("get_ang:%i, horiPresent:%i\n", Robot::Sensors::hori.get_angle(),
-         Robot::Sensors::hori.get_angle() != PROS_ERR);
-  pros::lcd::initialize();
-  pros::lcd::set_text(1, "Calibrating chassis...");
-  printf("<sensors = nullptr>\n");
-  if (Robot::odomSensors->horizontal1 == nullptr) printf("  hori1,\n");
-  if (Robot::odomSensors->horizontal2 == nullptr) printf("  hori2,\n");
-  if (Robot::odomSensors->imu == nullptr) printf("  imu,\n");
-  if (Robot::odomSensors->vertical1 == nullptr) printf("  vert1,\n");
-  if (Robot::odomSensors->vertical2 == nullptr) printf("  vert2,\n");
-  printf("<sensors = nullptr/>\n");
-  printf("<error>\n");
-  switch (errno) {
-    case PROS_ERR: printf("  PROS_ERR\n"); break;
-    case ENODEV: printf("  ENODEV\n"); break;
-    case ENXIO: printf("  ENXIO\n"); break;
-  }
-  errno = 0;
-  printf("<error/>\n");
-  Robot::chassis->calibrate(); // calibrate the chassis
-  pros::lcd::set_text(1, "Chassis Calibrated!");
-  Robot::chassis->setPose(0, 0, 0);
-  pros::Task screenTask(
-      screen); // create a task to print the position to the screen
+  // Robot::initializeOdometryConfig();
+  // printf("get_ang:%i, horiPresent:%i\n", Robot::Sensors::hori.get_angle(),
+  //        Robot::Sensors::hori.get_angle() != PROS_ERR);
+  // pros::lcd::initialize();
+  // pros::lcd::set_text(1, "Calibrating chassis...");
+  // printf("<sensors = nullptr>\n");
+  // if (Robot::odomSensors->horizontal1 == nullptr) printf("  hori1,\n");
+  // if (Robot::odomSensors->horizontal2 == nullptr) printf("  hori2,\n");
+  // if (Robot::odomSensors->imu == nullptr) printf("  imu,\n");
+  // if (Robot::odomSensors->vertical1 == nullptr) printf("  vert1,\n");
+  // if (Robot::odomSensors->vertical2 == nullptr) printf("  vert2,\n");
+  // printf("<sensors = nullptr/>\n");
+  // printf("<error>\n");
+  // switch (errno) {
+  //   case PROS_ERR: printf("  PROS_ERR\n"); break;
+  //   case ENODEV: printf("  ENODEV\n"); break;
+  //   case ENXIO: printf("  ENXIO\n"); break;
+  // }
+  // errno = 0;
+  // printf("<error/>\n");
+  // Robot::chassis->calibrate(); // calibrate the chassis
+  // pros::lcd::set_text(1, "Chassis Calibrated!");
+  // Robot::chassis->setPose(0, 0, 0);
+  // pros::Task screenTask(
+  //     screen); // create a task to print the position to the screen
 }
 
 /**
@@ -129,76 +129,76 @@ void competition_initialize() {}
  * from where it left off.
  */
 void autonomous() {
-  printf("auton start");
-  // score alliance triball
-  // lemlib::Logger::initialize();
-  // Robot::chassis->moveTo(60, -45, 0, 5000, false, true, 0, 0.6, 127, true);
+  // printf("auton start");
+  // // score alliance triball
+  // // lemlib::Logger::initialize();
+  // // Robot::chassis->moveTo(60, -45, 0, 5000, false, true, 0, 0.6, 127, true);
 
-  // shoot le ball
-  // Robot::chassis->setPose(-24, -24, 0);
-  // auton::actions::shootTriballIntoOffensiveZone();
+  // // shoot le ball
+  // // Robot::chassis->setPose(-24, -24, 0);
+  // // auton::actions::shootTriballIntoOffensiveZone();
 
-  // Robot::chassis->setPose((-72+Robot::Dimensions::drivetrainWidth/2 + 24),
-  // -72+Robot::Dimensions::drivetrainLength/2, 0);
-  using namespace fieldDimensions;
+  // // Robot::chassis->setPose((-72+Robot::Dimensions::drivetrainWidth/2 + 24),
+  // // -72+Robot::Dimensions::drivetrainLength/2, 0);
+  // using namespace fieldDimensions;
 
-  Robot::chassis->setPose(
-      -(fieldDimensions::MIN_X + TILE_LENGTH +
-        Robot::Dimensions::drivetrainWidth / 2),
-      (fieldDimensions::MIN_Y + Robot::Dimensions::drivetrainLength / 2), 0);
-  // Robot::Actions::expandWings();
-  // auton::actions::pushMatchLoadZoneTriball();
-  // pros::delay(500);
-  auton::actions::scoreAllianceTriball();
+  // Robot::chassis->setPose(
+  //     -(fieldDimensions::MIN_X + TILE_LENGTH +
+  //       Robot::Dimensions::drivetrainWidth / 2),
+  //     (fieldDimensions::MIN_Y + Robot::Dimensions::drivetrainLength / 2), 0);
+  // // Robot::Actions::expandWings();
+  // // auton::actions::pushMatchLoadZoneTriball();
+  // // pros::delay(500);
+  // auton::actions::scoreAllianceTriball();
 
-  // auton::actions::touchElevationBar();
-  // Robot::chassis->moveTo(0 + TILE_RADIUS, MIN_Y + (TILE_LENGTH * 2), LEFT,
-  //                        3000);
-  // Robot::Actions::intake();
-  // Robot::chassis->tank(96, 96);
-  // pros::delay(500);
-  // Robot::Actions::stopIntake();
-  // Robot::chassis->turnTo(0, 10000, 3000);
-  // Robot::chassis->moveTo(0 + Robot::Dimensions::drivetrainWidth/2 - 1, MIN_Y + (TILE_LENGTH * 2.5), UP,
-  //                        3000);
-  // Robot::Actions::expandWings();
-  // Robot::chassis->turnTo(10000, 0, 3000);
-  // Robot::Actions::outtake();
-  // pros::delay(150);
-  // Robot::chassis->tank(127, 127);
-  // pros::delay(800);
-  // Robot::Actions::retractWings();
-  // Robot::Actions::stopIntake();
-  // Robot::chassis->tank(-64, -60);
-  // pros::delay(175);
+  // // auton::actions::touchElevationBar();
+  // // Robot::chassis->moveTo(0 + TILE_RADIUS, MIN_Y + (TILE_LENGTH * 2), LEFT,
+  // //                        3000);
+  // // Robot::Actions::intake();
+  // // Robot::chassis->tank(96, 96);
+  // // pros::delay(500);
+  // // Robot::Actions::stopIntake();
+  // // Robot::chassis->turnTo(0, 10000, 3000);
+  // // Robot::chassis->moveTo(0 + Robot::Dimensions::drivetrainWidth/2 - 1, MIN_Y + (TILE_LENGTH * 2.5), UP,
+  // //                        3000);
+  // // Robot::Actions::expandWings();
+  // // Robot::chassis->turnTo(10000, 0, 3000);
+  // // Robot::Actions::outtake();
+  // // pros::delay(150);
+  // // Robot::chassis->tank(127, 127);
+  // // pros::delay(800);
+  // // Robot::Actions::retractWings();
+  // // Robot::Actions::stopIntake();
+  // // Robot::chassis->tank(-64, -60);
+  // // pros::delay(175);
 
   
-  auton::actions::touchElevationBar();
+  // auton::actions::touchElevationBar();
 
-  // move forward one tile
-  // Robot::chassis->setPose(0, 0, 0);
-  // Robot::chassis->moveTo(0, 24, 0, 2000);
+  // // move forward one tile
+  // // Robot::chassis->setPose(0, 0, 0);
+  // // Robot::chassis->moveTo(0, 24, 0, 2000);
 
-  // // move right one tile
-  // Robot::chassis.moveTo(-12, 0, 5000);
+  // // // move right one tile
+  // // Robot::chassis.moveTo(-12, 0, 5000);
 
-  // // move right and forward one tile
-  // Robot::chassis->moveTo(0, 24, 0,5000);
+  // // // move right and forward one tile
+  // // Robot::chassis->moveTo(0, 24, 0,5000);
 
-  // turn 90 deg
-  // Robot::chassis->turnTo(0, -1000, 5000);
-  // // turn around
-  // Robot::chassis.turnTo(0, -24, 5000);
+  // // turn 90 deg
+  // // Robot::chassis->turnTo(0, -1000, 5000);
+  // // // turn around
+  // // Robot::chassis.turnTo(0, -24, 5000);
 
-  // // move in square
-  // Robot::chassis.moveTo(0, 24, 500);
-  // pros::delay(1000);
-  // Robot::chassis.moveTo(24, 24, 500);
-  // pros::delay(1000);
-  // Robot::chassis.moveTo(24, 0, 500);
-  // pros::delay(1000);
-  // Robot::chassis.moveTo(0, 0, 500);
-  // pros::delay(1000);
+  // // // move in square
+  // // Robot::chassis.moveTo(0, 24, 500);
+  // // pros::delay(1000);
+  // // Robot::chassis.moveTo(24, 24, 500);
+  // // pros::delay(1000);
+  // // Robot::chassis.moveTo(24, 0, 500);
+  // // pros::delay(1000);
+  // // Robot::chassis.moveTo(0, 0, 500);
+  // // pros::delay(1000);
 }
 
 /**
@@ -252,10 +252,10 @@ void opcontrol() {
    *
    * true = expanded
    */
-  bool intakeElevatorState = false;
-  bool wasXPressed = false;
-  bool wingsState = false;
-  bool wasUpPressed = false;
+  // bool intakeElevatorState = false;
+  // bool wasXPressed = false;
+  // bool wingsState = false;
+  // bool wasUpPressed = false;
   // int shooterState = 0;
   // bool wasShootPressed = false;
   while (true) {
@@ -270,16 +270,16 @@ void opcontrol() {
     //     pow(Robot::control.getAnalog(ControllerAnalog::rightY), 3) * 12000);
     // Robot::chassis.curvature(Robot::control.getAnalog(ControllerAnalog::leftY)*127,
     // Robot::control.getAnalog(ControllerAnalog::rightX)*127,20);
-    Robot::chassis->tank(
-        Robot::control.getAnalog(ControllerAnalog::leftY) * 127,
-        Robot::control.getAnalog(ControllerAnalog::rightY) * 127, 15);
+    // Robot::chassis->tank(
+    //     Robot::control.getAnalog(ControllerAnalog::leftY) * 127,
+    //     Robot::control.getAnalog(ControllerAnalog::rightY) * 127, 15);
 
-    // intake / outtake
-    if (Robot::control.getDigital(ControllerDigital::L1))
-      Robot::Motors::intake.move(127);
-    else if (Robot::control.getDigital(ControllerDigital::L2))
-      Robot::Motors::intake.move(-127);
-    else Robot::Motors::intake.move(0);
+    // // intake / outtake
+    // if (Robot::control.getDigital(ControllerDigital::L1))
+    //   Robot::Motors::intake.move(127);
+    // else if (Robot::control.getDigital(ControllerDigital::L2))
+    //   Robot::Motors::intake.move(-127);
+    // else Robot::Motors::intake.move(0);
 
     // // shoot / un-shoot?
     if (Robot::control.getDigital(ControllerDigital::R1)) {
@@ -303,35 +303,35 @@ void opcontrol() {
     // else wasShootPressed = false;
 
     // elevate intake
-    if (Robot::control.getDigital(ControllerDigital::X)) {
-      if (!wasXPressed)
-        Robot::Pistons::intakeElevator.set_value(intakeElevatorState ^= true);
-      wasXPressed = true;
-    } else wasXPressed = false;
+    // if (Robot::control.getDigital(ControllerDigital::X)) {
+    //   if (!wasXPressed)
+    //     Robot::Pistons::intakeElevator.set_value(intakeElevatorState ^= true);
+    //   wasXPressed = true;
+    // } else wasXPressed = false;
 
-    if (Robot::control.getDigital(ControllerDigital::up)) {
-      if (!wasUpPressed) Robot::Pistons::wings.set_value(wingsState ^= true);
-      wasUpPressed = true;
-    } else wasUpPressed = false;
+    // if (Robot::control.getDigital(ControllerDigital::up)) {
+    //   if (!wasUpPressed) Robot::Pistons::wings.set_value(wingsState ^= true);
+    //   wasUpPressed = true;
+    // } else wasUpPressed = false;
 
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     // 							     Driver Feedback
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     // drive temp display
-    const auto leftTempArr = Robot::Motors::leftDrive.get_temperatures();
-    const auto rightTempArr = Robot::Motors::rightDrive.get_temperatures();
+    // const auto leftTempArr = Robot::Motors::leftDrive.get_temperatures();
+    // const auto rightTempArr = Robot::Motors::rightDrive.get_temperatures();
 
-    const std::string leftTempStr =
-        std::to_string((int)((leftTempArr[0] + leftTempArr[1]) * 0.9) + 32);
-    const std::string rightTempStr =
-        std::to_string((int)((rightTempArr[0] + rightTempArr[1]) * 0.9) + 32);
+    // const std::string leftTempStr =
+    //     std::to_string((int)((leftTempArr[0] + leftTempArr[1]) * 0.9) + 32);
+    // const std::string rightTempStr =
+    //     std::to_string((int)((rightTempArr[0] + rightTempArr[1]) * 0.9) + 32);
 
-    Robot::control.clearLine(0);
-    Robot::control.setText(
-        0, 0,
-        leftTempStr + std::string(4 - leftTempStr.length(), ' ') + "DRIVE " +
-            std::string(4 - rightTempStr.length(), ' ') + rightTempStr);
+    // Robot::control.clearLine(0);
+    // Robot::control.setText(
+    //     0, 0,
+    //     leftTempStr + std::string(4 - leftTempStr.length(), ' ') + "DRIVE " +
+    //         std::string(4 - rightTempStr.length(), ' ') + rightTempStr);
 
     pros::delay(20);
   }
