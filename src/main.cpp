66ea279c66661d9dd2dfff2ba65e8af736ffbf51
meisZWFLZ <atcurtis08@gmail.com>
2023-12-11 17:59:47 -0700

    style: :coffin: remove those useless annoying comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,15 @@
 #include "main.h"
-#include "pros/misc.hpp"
 #include "robot.h"
 #include "auton.h"
-#include <numeric>
 #include <string>
-#include <vector>
 #include "neil_pid.h"
-#include <vector>
 #include "fieldDimensions.h"
 #include "selector.h"
 
 bool autonHasRun = false;
 
-float average(std::vector<float> const& v) {
-  if (v.empty()) { return 0; }
-
-  auto const count = static_cast<float>(v.size());
-  return std::reduce(v.begin(), v.end()) / count;
-}
-
-const float get_wattage(pros::Motor_Group& group) {
-  std::vector<float> watts = {};
-  for (int i = 0; i < group.size(); i++) watts.push_back(group[i].get_power());
-  return average(watts);
-}
-
 void screen() {
   // loop forever
-  pros::Controller controller(pros::controller_id_e_t::E_CONTROLLER_MASTER);
   while (true) {
     lemlib::Pose pose =
         Robot::chassis->getPose(); // get the current position of the robot
@@ -38,28 +20,6 @@ void screen() {
     pros::lcd::clear_line(3);
     pros::lcd::print(3, "heading: %f deg",
                      pose.theta); // print the heading
-    if (!controller.get_digital(
-            pros::controller_digital_e_t::E_CONTROLLER_DIGITAL_UP)) {
-    } else {
-      // display wattage
-      // controller.clear_line(0);
-      printf("in: %fw\n", get_wattage(Robot::Motors::intake));
-      // char intake[15];
-      // sprintf(intake, "in: %fw",
-      //         get_wattage(Robot::Motors::intake)); // print the x position
-      // controller.set_text(0, 0, intake);
-      // // controller.clear_line(1);
-      // char shooter[15];
-      // sprintf(shooter, "fly: %fw",
-      //         get_wattage(Robot::Motors::shooter)); // print the x position
-      // controller.set_text(0, 0, shooter);
-      // char drive[15];
-      // sprintf(drive, "dri: %fw",
-      //         (get_wattage(Robot::Motors::leftDrive) +
-      //          get_wattage(Robot::Motors::rightDrive)) /
-      //             2); // print the x position
-      // controller.set_text(2, 0, drive); // print the heading
-    }
     pros::delay(200);
   }
 }
@@ -80,25 +40,10 @@ void addAutons() {
  */
 void initialize() {
   Robot::initializeOdometryConfig();
-  // printf("get_ang:%i, horiPresent:%i\n", Robot::Sensors::hori.get_angle(),
-  //        Robot::Sensors::hori.get_angle() != PROS_ERR);
+
   pros::lcd::initialize();
   pros::lcd::set_text(1, "Calibrating chassis...");
-  // printf("<sensors = nullptr>\n");
-  // if (Robot::odomSensors->horizontal1 == nullptr) printf("  hori1,\n");
-  // if (Robot::odomSensors->horizontal2 == nullptr) printf("  hori2,\n");
-  // if (Robot::odomSensors->imu == nullptr) printf("  imu,\n");
-  // if (Robot::odomSensors->vertical1 == nullptr) printf("  vert1,\n");
-  // if (Robot::odomSensors->vertical2 == nullptr) printf("  vert2,\n");
-  // printf("<sensors = nullptr/>\n");
-  // printf("<error>\n");
-  // switch (errno) {
-  //   case PROS_ERR: printf("  PROS_ERR\n"); break;
-  //   case ENODEV: printf("  ENODEV\n"); break;
-  //   case ENXIO: printf("  ENXIO\n"); break;
-  // }
-  // errno = 0;
-  // printf("<error/>\n");
+
   Robot::chassis->calibrate(); // calibrate the chassis
   pros::lcd::set_text(1, "Chassis Calibrated!");
   Robot::chassis->setPose(0, 0, 0);
@@ -128,44 +73,6 @@ void disabled() {}
  */
 void competition_initialize() {}
 
-void score4BallAuto() {
-  using namespace fieldDimensions;
-  // pick up and shoot first triball
-  Robot::chassis->moveTo(0 + TILE_RADIUS, MIN_Y + (TILE_LENGTH * 2), LEFT - 15,
-                         3000);
-  Robot::Actions::intake();
-  Robot::Actions::shoot();
-  Robot::chassis->tank(96, 96);
-  pros::delay(500);
-  Robot::Actions::stopIntake();
-  Robot::Actions::stopShooter();
-
-  // pick up second triball
-  Robot::chassis->moveTo(0 + TILE_RADIUS, MIN_Y + (TILE_LENGTH * 3), LEFT,
-                         3000);
-  Robot::Actions::intake();
-  Robot::chassis->tank(96, 96);
-  pros::delay(500);
-  Robot::Actions::stopIntake();
-  Robot::chassis->turnTo(0, 10000, 3000);
-
-  // Robot::chassis->moveTo(0 + Robot::Dimensions::drivetrainWidth/2 - 1, MIN_Y
-  // + (TILE_LENGTH * 2.5), UP,
-  //                        3000);
-
-  // Plow triballs into goal
-  Robot::Actions::expandWings();
-  Robot::chassis->turnTo(10000, 0, 3000);
-  Robot::Actions::outtake();
-  pros::delay(150);
-  Robot::chassis->tank(127, 127);
-  pros::delay(800);
-  Robot::Actions::retractWings();
-  Robot::Actions::stopIntake();
-  Robot::chassis->tank(-64, -60);
-  pros::delay(175);
-}
-
 void intakeAndShoot() {
   using namespace fieldDimensions;
 
@@ -194,43 +101,6 @@ void autonomous() {
   pros::competition::is_connected();
 
   auton::AutonSelector::runAuton();
-  // score alliance triball
-  // lemlib::Logger::initialize();
-  // Robot::chassis->setPose(41.5, -65.125, 0);
-  // Robot::chassis->moveTo(60, -45, 0, 5000, false, true, 0, 0.6, 127, true);
-
-  // shoot le ball
-  // Robot::chassis->setPose(-24, -24, 0);
-  // auton::actions::shootTriballIntoOffensiveZone();
-
-  // Robot::chassis->setPose((-72+Robot::Dimensions::drivetrainWidth/2 + 24),
-  // -72+Robot::Dimensions::drivetrainLength/2, 0);
-  // auton::actions::touchElevationBar();
-  // auton::actions::scoreAllianceTriball();
-
-  // move forward one tile
-  // Robot::chassis.moveTo(0, 24, 5000, 200);
-
-  // // move right one tile
-  // Robot::chassis.moveTo(-12, 0, 5000);
-
-  // // move right and forward one tile
-  // Robot::chassis->moveTo(0, 24, 0,5000);
-
-  // turn 90 deg
-  // Robot::chassis.turnTo(1000, 0, 5000);
-  // // turn around
-  // Robot::chassis.turnTo(0, -24, 5000);
-
-  // // move in square
-  // Robot::chassis.moveTo(0, 24, 500);
-  // pros::delay(1000);
-  // Robot::chassis.moveTo(24, 24, 500);
-  // pros::delay(1000);
-  // Robot::chassis.moveTo(24, 0, 500);
-  // pros::delay(1000);
-  // Robot::chassis.moveTo(0, 0, 500);
-  // pros::delay(1000);
 }
 
 /**
@@ -252,11 +122,6 @@ void opcontrol() {
 
   if (pros::competition::is_connected() && !autonHasRun)
     Robot::Actions::prepareIntake();
-
-  // using namespace fieldDimensions;
-  // Robot::chassis->setPose(
-  //     (MIN_X + TILE_LENGTH + Robot::Dimensions::drivetrainWidth / 2),
-  //     (MIN_Y + Robot::Dimensions::drivetrainLength / 2), UP);
 
   bool skills = false;
   if (!std::strcmp(auton::AutonSelector::getCurrentAuton(), (char*)("skills")))
@@ -282,80 +147,29 @@ void opcontrol() {
   bool wingsState = false;
   bool wasUpPressed = false;
 
-  // disable drive
-  // bool macroRunning = false;
-  // pros::Task* macroTask = nullptr;
   bool prevR1 = Robot::control.getDigital(ControllerDigital::R1);
   bool prevR2 = Robot::control.getDigital(ControllerDigital::R2);
   int shooterState = 0;
-  // bool wasShootPressed = false;
   while (true) {
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     // 								     Drive Code
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     // drivetrain
-    // Robot::Motors::leftDrive.move_voltage(
-    //     pow(Robot::control.getAnalog(ControllerAnalog::leftY), 3) * 12000);
-    // Robot::Motors::rightDrive.move_voltage(
-    //     pow(Robot::control.getAnalog(ControllerAnalog::rightY), 3) * 12000);
-    // Robot::chassis.curvature(Robot::control.getAnalog(ControllerAnalog::leftY)*127,
-    // Robot::control.getAnalog(ControllerAnalog::rightX)*127,20);
-    // if (macroTask == nullptr) {
-    //   macroRunning = false;
-    //   printf("macro null\n");
-    // }
-    // if (macroTask != nullptr) printf("state:%i \n", macroTask->get_state());
-    // if ((macroRunning &&
-    //      (std::abs(Robot::control.getAnalog(ControllerAnalog::leftY)) > 0.1
-    //      ||
-    //       std::abs(Robot::control.getAnalog(ControllerAnalog::rightY)) >
-    //           0.1)) ||
-    //     (macroTask != nullptr &&
-    //      macroTask->get_state() == pros::E_TASK_STATE_SUSPENDED)) {
-    //   macroRunning = false;
-    //   macroTask->suspend();
-    //   macroTask->remove();
-    //   // delete &macroTask;
-    //   macroTask = nullptr;
-    // }
-    // if (!macroRunning)
     Robot::chassis->tank(
         Robot::control.getAnalog(ControllerAnalog::leftY) * 127,
         Robot::control.getAnalog(ControllerAnalog::rightY) * 127, 15);
 
     // intake / outtake
-    // if (!macroRunning) {
     if (Robot::control.getDigital(ControllerDigital::L1))
       Robot::Motors::intake.move(127);
     else if (Robot::control.getDigital(ControllerDigital::L2))
       Robot::Motors::intake.move(-127);
     else Robot::Motors::intake.move(0);
-    // }
 
     // // shoot / un-shoot? / shootMacro
     const bool r1 = Robot::control.getDigital(ControllerDigital::R1);
     const bool r2 = Robot::control.getDigital(ControllerDigital::R2);
-    // printf("run shooter macro");
-    // if (!(prevR1 && prevR2) && r1 && r2) {
-    //   printf("run shooter macro\n");
-    //   // pros::Task::delay(1000);
-    //   if (!macroRunning && macroTask == nullptr) {
-    //     printf("start shooter macro\n");
-    //     macroRunning = true;
-    //     macroTask =
-    //         new pros::Task(Robot::Actions::shootMacro, TASK_PRIORITY_DEFAULT,
-    //                        TASK_STACK_DEPTH_DEFAULT, "shooterMacro");
-    //   } else if (macroRunning && macroTask != nullptr) {
-    //     printf("stop shooter macro\n");
-    //     macroRunning = false;
-    //     macroTask->suspend();
-    //     macroTask->remove();
-    //     // delete &macroTask;
-    //     macroTask = nullptr;
-    //   }
-    // }
-    // if (!macroRunning && !(r1 && r2)) {
 
     if (skills) {
       if (r1 && prevR1 == false) shooterState = shooterState == 0 ? 1 : 0;
@@ -372,21 +186,6 @@ void opcontrol() {
       else if (r2) Robot::Actions::unshoot();
       else Robot::Actions::stopShooter();
     }
-    // }
-    // if (Robot::control.getDigital(ControllerDigital::R1)) {
-    //   if(!wasShootPressed) {
-    //     Robot::Motors::shooter.move(shooterState = shooterState != 0 ? 0 :
-    //     127); Robot::control.rumble("-");
-    //   }
-    //   wasShootPressed = true;
-    // } else if (Robot::control.getDigital(ControllerDigital::R2)) {
-    //   if(!wasShootPressed) {
-    //     Robot::Motors::shooter.move(shooterState = shooterState != 0 ? 0 :
-    //     -127); Robot::control.rumble("-");
-    //   }
-    //   wasShootPressed = true;
-    // }
-    // else wasShootPressed = false;
 
     // elevate intake
     if (Robot::control.getDigital(ControllerDigital::X)) {
@@ -402,26 +201,6 @@ void opcontrol() {
       if (!wasUpPressed) Robot::Pistons::wings.set_value(wingsState ^= true);
       wasUpPressed = true;
     } else wasUpPressed = false;
-
-    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    // 							     Driver Feedback
-    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-    // // drive temp display
-    // const auto leftTempArr = Robot::Motors::leftDrive.get_temperatures();
-    // const auto rightTempArr = Robot::Motors::rightDrive.get_temperatures();
-
-    // const std::string leftTempStr =
-    //     std::to_string((int)((leftTempArr[0] + leftTempArr[1]) * 0.9) + 32);
-    // const std::string rightTempStr =
-    //     std::to_string((int)((rightTempArr[0] + rightTempArr[1]) * 0.9) +
-    //     32);
-
-    // Robot::control.clearLine(0);
-    // Robot::control.setText(
-    //     0, 0,
-    //     leftTempStr + std::string(4 - leftTempStr.length(), ' ') + "DRIVE " +
-    //         std::string(4 - rightTempStr.length(), ' ') + rightTempStr);
 
     pros::delay(20);
   }
