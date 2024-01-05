c1a2ab9bb1ed2d2df0353649cf84cebb7e5ec150
meisZWFLZ <atcurtis08@gmail.com>
2024-01-04 22:57:49 -0700

    feat: :construction: 6 ball auton

--- src/auton/autons/offensive.cpp
+++ src/auton/autons/offensive.cpp
@@ -1,42 +1,91 @@
 #include "auton.h"
+#include "lemlib/util.hpp"
 #include "pros/rtos.hpp"
 #include "robot.h"
 #include "fieldDimensions.h"
 
 using namespace fieldDimensions;
 
-ASSET(off_score_alliance_txt);
-ASSET(off_elevation_bar_txt);
+ASSET(ball6_matchload_sweep_txt);
+
+void tank(float left, float right, int ms) {
+  Robot::chassis->tank(left, right);
+  pros::delay(ms);
+}
+
+void waitUntilDistTarget(lemlib::Pose target, float error = 3, int time = 300) {
+  printf("start wait\n");
+  const int start = pros::millis();
+  int inRangeStartTime = 0;
+  pros::delay(100);
+  do {
+    if (inRangeStartTime == 0) {
+      if (Robot::chassis->getPose().distance(target) < error)
+        inRangeStartTime = pros::millis();
+    } else if (pros::millis() - inRangeStartTime > time) {
+      printf("breaking\n");
+      break;
+    }
+    pros::delay(10);
+  } while (true);
+  printf("time in wait: %i\n", pros::millis() - start);
+}
 
 void runOffensive() {
-  Robot::chassis->setPose(rightStartingPose, false);
-  // @todo add offensive auton
+  Robot::chassis->setPose(
+      {0 + TILE_LENGTH - Robot::Dimensions::drivetrainLength / 2,
+       MIN_Y + TILE_RADIUS, LEFT},
+      false);
 
-  Robot::chassis->follow(off_score_alliance_txt, 10, 2000);
-  Robot::chassis->waitUntil(25);
-  Robot::Actions::outtake();
+  // intake triball
+  Robot::Actions::intake();
+  tank(36, 36, 75);
+  tank(72, 72, 200);
+  tank(36, 36, 75);
+  tank(0, 0, 50);
+
+  // get away from scary zone
+  tank(-36, -36, 75);
+  tank(-72, -72, 200);
+  tank(-36, -36, 75);
+  tank(0, 0, 50);
+  Robot::Motors::intake.move_voltage(40);
+
+  // // start turn
+  // tank(127, -127, 100);
+  // turn towards the path
+  Robot::chassis->turnTo(10000000, 0, 400);
+
+  // while (lemlib::angleError(Robot::chassis->getPose().theta, RIGHT, false) >
+  // 10)
+  //   pros::delay(10);
+  // Robot::chassis->cancelMotion();
+
   Robot::chassis->waitUntilDone();
+  printf("cancel turn\n");
+  Robot::chassis->follow(ball6_matchload_sweep_txt, 13, 5000);
 
-  // ram it again
-  Robot::chassis->tank(-127, -127);
-  pros::delay(250);
-  Robot::chassis->tank(127, 127);
+  waitUntilDistTarget({MAX_X - TILE_LENGTH, MIN_Y + TILE_LENGTH - 3}, 6,
+                      250);
+  Robot::Actions::expandWings();
+  printf("expand wings\n");
+
   pros::delay(500);
-  // get outta there
-  Robot::chassis->tank(-127, -127);
+  Robot::Actions::retractWings();
+  printf("retract wings\n");
+
+  Robot::chassis->waitUntil(60);
+  Robot::Actions::outtake();
+  printf("outtake\n");
+
+  waitUntilDistTarget({MAX_X - TILE_RADIUS, -TILE_LENGTH}, 8, 750);
+  Robot::chassis->cancelMotion();
+  printf("stop follow\n");
+
+  tank(-127, -127, 100);
   Robot::Actions::stopIntake();
-  pros::delay(400);
-
-  Robot::chassis->turnTo(-10000, 0, 1000);
-  Robot::chassis->waitUntilDone();
-
-  Robot::chassis->follow(off_elevation_bar_txt, 10, 2750);
-  Robot::chassis->waitUntilDone();
-
-  Robot::chassis->tank(127, 127);
-  // Robot::Actions::lowerIntake();
-  pros::delay(400);
-  Robot::chassis->tank(0, 0);
+  tank(0, 0, 0);
+  printf("stop\n");
 }
 
 auton::Auton auton::autons::offensive = {(char*)("offensive / right"),
