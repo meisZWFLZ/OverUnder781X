d5c3846bf8f29566dd4b8c2e467342c32c156705
meisZWFLZ <atcurtis08@gmail.com>
2024-01-08 12:37:28 -0700

    feat: :sparkles: working offensive 5 ball-ish auto

--- src/auton/autons/sixBall.cpp
+++ src/auton/autons/sixBall.cpp
@@ -11,6 +11,16 @@ using namespace auton::utils;
 ASSET(ball6_matchload_sweep_txt);
 ASSET(ball6_center_plow_1_txt);
 
+float roundDegrees(float ang) { return std::abs(std::remainder(ang, 360)); }
+
+float angDiff(float ang1, float ang2) {
+  return roundDegrees(roundDegrees(ang1) - roundDegrees(ang2));
+}
+
+float robotAngDiff(float ang) {
+  return std::abs(angDiff(Robot::chassis->getPose().theta, ang));
+}
+
 void run6Ball() {
   Robot::chassis->setPose(
       {0 + TILE_LENGTH - Robot::Dimensions::drivetrainLength / 2,
@@ -19,71 +29,103 @@ void run6Ball() {
 
   // intake triball
   Robot::Actions::intake();
-  tank(127, 127, 200);
+  tank(127, 127, 150, 0);
 
   // get away from other quadrant and begin turning
-  tank(-64, -127, 200);
+  tank(0, 0, 180, 127.0 / 18);
+  pros::delay(100);
+  tank(-127, -127, 220, 127.0 / 18);
+  tank(127, -127, 200, 0);
 
   // dont constantly spin the intake which could damage the motor
-  Robot::Actions::stopIntake();
-
   printf("intook triball\n");
 
   // turn to face right side of field (I use (10000000,0) as the target since
   // its basically a turn to 90 deg)
   Robot::chassis->turnTo(10000000, 0, 400);
   Robot::chassis->waitUntilDone();
+  Robot::Actions::stopIntake();
   printf("turn done\n");
 
   // Path to smoothly remove triball in matchload zone and from the matchload
   // zone and plow the three balls into the goal
-  Robot::chassis->follow(ball6_matchload_sweep_txt, 13, 2500);
+  Robot::chassis->follow(ball6_matchload_sweep_txt, 13, 2750);
 
   // wait until the robot is near the matchload zone to expand wings
-  waitUntilDistToPose({MAX_X - TILE_LENGTH, MIN_Y + TILE_LENGTH - 3}, 6, 250);
+  waitUntilDistToPose({MAX_X - TILE_LENGTH, MIN_Y + TILE_LENGTH - 3}, 6, 0,
+                      true);
   Robot::Actions::expandWings();
   printf("expand wings\n");
+  pros::delay(200);
+  Robot::chassis->cancelMotion();
 
   // retract wings soon after removing the triball from the matchload zone
-  pros::delay(500);
+  tank(24, 36, 200);
   Robot::Actions::retractWings();
   printf("retract wings\n");
+  tank(24, 36, 200);
+
+  printf("back to follow\n");
+
+  Robot::chassis->follow(ball6_matchload_sweep_txt, 13, 2750);
 
   // wait until the robot is near the goal to outtake
-  Robot::chassis->waitUntil(60);
+
+  pros::delay(450);
   Robot::Actions::outtake();
   printf("outtake\n");
 
+  while ((std::abs(Robot::chassis->getPose().x - (MAX_X - TILE_RADIUS)) > 10 ||
+          robotAngDiff(0) > 20) &&
+         Robot::chassis->isInMotion() &&
+         Robot::chassis->getPose().theta < 420) {
+    pros::delay(10);
+  }
+  printf("cancel follow\n");
   // fully ram into goal
-  tank(127, 127, 100, 0);
+  Robot::chassis->cancelMotion();
+  tank(127, 100, 0, 0);
+  waitUntilDistToPose({MAX_X - TILE_RADIUS, -TILE_LENGTH}, 10, 0, false);
+  pros::delay(100);
+  tank(-127, -127, 200, 0);
+  tank(90, 127, 500, 0);
+  pros::delay(150);
 
-  // stop ramming
-  stop();
+  Robot::chassis->setPose(
+      {Robot::chassis->getPose().x + 3.5f,
+       0 - TILE_LENGTH - Robot::Dimensions::drivetrainLength / 2 + 5, UP},
+      false);
 
+  tank(0, 0, 0, 0);
   // back out of goal and turn towards next path
-  tank(-127, -64, 100);
+  tank(-127, -92, 400, 127.0 / 18);
   Robot::Actions::stopIntake();
 
   // intake triball closest to elevation pole
-  const lemlib::Pose intakeCloseTriballTarget {
-      0 + Robot::Dimensions::drivetrainLength / 2 + 2, 0 - TILE_LENGTH, LEFT};
-  Robot::chassis->moveToPose(intakeCloseTriballTarget.x,
-                             intakeCloseTriballTarget.y,
-                             intakeCloseTriballTarget.theta, 2000);
+  // Robot::chassis->moveToPoint(TILE_LENGTH * 1.5, -TILE_LENGTH * 1.5, 1000);
+  // waitUntilDistToPose({TILE_LENGTH * 1.5, -TILE_LENGTH * 1.5, 0}, 8, 0,
+  // true);
+
+  const lemlib::Pose intakeCloseTriballTarget {0 + 3, 0 - TILE_LENGTH - 2,
+                                               LEFT};
+
+  Robot::chassis->moveToPose(
+      intakeCloseTriballTarget.x, intakeCloseTriballTarget.y,
+      intakeCloseTriballTarget.theta, 2500, {.lead = 0.5});
 
   // Wait until close to the triball to intake. This prevents the robot from
   // intaking a triball that has been flung over from another robot
-  waitUntilDistToPose(intakeCloseTriballTarget, 5, 0);
+  waitUntilDistToPose(intakeCloseTriballTarget, 12, 0, true);
   Robot::Actions::intake();
 
   // wait until triball is intaked
-  Robot::chassis->waitUntilDone();
-
+  waitUntilDistToPose(intakeCloseTriballTarget, 8, 100, true);
+  Robot::chassis->cancelMotion();
   // turn to face the triball in the center of the field
-  tank(0, -127, 150);
+  tank(-64, -127, 200, 0);
 
   // push currently held triball and center triball into goal
-  Robot::chassis->follow(ball6_center_plow_1_txt, 10, 2000);
+  Robot::chassis->follow(ball6_center_plow_1_txt, 10, 3000);
 
   // wait until triball is fully intaked to stop
   pros::delay(300);
@@ -94,64 +136,23 @@ void run6Ball() {
   Robot::Actions::expandWings();
 
   // wait until near the goal to outtake
-  waitUntilDistToPose({2 * TILE_LENGTH, 0}, 10, 0);
+  Robot::chassis->waitUntil(24);
   Robot::Actions::outtake();
 
   // wait until path follow is done to stop intake and retract wings
   Robot::chassis->waitUntilDone();
+  tank(127, 127, 100, 0);
   Robot::Actions::stopIntake();
   Robot::Actions::retractWings();
 
   // turn to face last triball
   tank(-64, -127, 200);
 
-  // intake last triball
-  const lemlib::Pose intakeLastTriballTarget {intakeCloseTriballTarget.x, 0,
-                                              LEFT};
-  Robot::chassis->moveToPose(intakeLastTriballTarget.x,
-                             intakeLastTriballTarget.y,
-                             intakeLastTriballTarget.theta, 2000);
-
-  // Wait until close to the triball to intake. This prevents the robot from
-  // intaking a triball that has been flung over from another robot
-  waitUntilDistToPose(intakeLastTriballTarget, 5, 0);
-  Robot::Actions::intake();
-
-  // wait until triball is intaked
+  Robot::chassis->moveToPoint(TILE_RADIUS - 1.2, -TILE_LENGTH * 1.5 - 0.5, 2000);
+  Robot::chassis->turnTo(-1 * pow(10, 6), .68 * pow(10, 6), 1000, true, 64);
+  pros::delay(40);
+  Robot::Actions::expandWings();
   Robot::chassis->waitUntilDone();
-
-  // turn to face the goal
-  tank(-127, -64, 200);
-
-  Robot::chassis->turnTo(1000000, 0, 1000);
-
-  // give time to intake
-  pros::delay(200);
-  Robot::Actions::stopIntake();
-
-  // once within 10 degrees of facing towards the goal, stop turning
-  while (Robot::chassis->isInMotion() &&
-         lemlib::angleError(Robot::chassis->getPose().theta, RIGHT) > 10) {
-    pros::delay(10);
-  }
-  Robot::chassis->cancelMotion();
-
-  // full power ram into goal
-  tank(127, 127, 350);
-
-  // wait until near goal to outtake
-  waitUntilDistToPose({TILE_LENGTH * 1.5, 0}, 10);
-  Robot::Actions::outtake();
-
-  // wait until at goal to stop
-  waitUntilDistToPose({TILE_LENGTH * 2, 0}, 10, 400);
-  stop();
-
-  // back out of goal and turn towards elevation pole
-  tank(-64, -127, 150);
-
-  // touch elevation bar
-
   // outtake
   printf("stop\n");
 }
