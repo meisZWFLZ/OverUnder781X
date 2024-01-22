6fccaf0e7e2bf0c79ce04dff294dcd70e682fc8b
meisZWFLZ <atcurtis08@gmail.com>
2024-01-22 16:20:48 -0700

    fix: :bug: change prepare for matchloading for new set

--- src/auton/actions/prepareForMatchLoading.cpp
+++ src/auton/actions/prepareForMatchLoading.cpp
@@ -1,16 +1,19 @@
 #include "auton.h"
+#include "pros/motors.h"
 #include "robot.h"
 #include "fieldDimensions.h"
 
 using namespace fieldDimensions;
+using namespace auton::utils;
 
 void auton::actions::prepareForMatchloading() {
-   Robot::chassis->moveToPose(MIN_X + TILE_LENGTH - 8, MIN_Y + TILE_LENGTH - 2,
-                         LEFT - 22.5, 3000);
-
-  Robot::chassis->waitUntil(6);
-  Robot::Actions::shoot();
-  Robot::chassis->waitUntilDone();
+  tank(-64, -64, 0, 0);
+  // Robot::Motors::rightDrive.set_brake_modes(pros::E_MOTOR_BRAKE_BRAKE);
+  // Robot::Motors::rightDrive.brake();
+  waitUntil([] {
+    const float angDist = robotAngDist(40);
+    printf("angDist: %f\n", angDist);
+    return angDist < 25;
+  });
   // Robot::Actions::lowerIntake();
-
 }
