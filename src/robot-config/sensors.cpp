b30d75e92f18b29a2f0af80b5d1577fc6928ca07
meisZWFLZ <atcurtis08@gmail.com>
2024-02-17 10:59:08 -0700

    fix: :alien: imuC port

--- src/robot-config/sensors.cpp
+++ src/robot-config/sensors.cpp
@@ -6,6 +6,6 @@ pros::Rotation Robot::Sensors::vert {14, false};
 pros::Rotation Robot::Sensors::hori {7, false};
 pros::Imu Robot::Sensors::imuA {3};
 pros::Imu Robot::Sensors::imuB {4};
-pros::Imu Robot::Sensors::imuC {5};
+pros::Imu Robot::Sensors::imuC {16};
 pros::ADILineSensor Robot::Sensors::cataTriball {'H'};
 pros::Rotation Robot::Sensors::cata {15};
