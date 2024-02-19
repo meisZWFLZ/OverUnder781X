7cb9d4b984a9497bc755da17397f5f2eea6516a6
meisZWFLZ <atcurtis08@gmail.com>
2024-02-18 17:33:30 -0700

    fix: :alien: reverse horizontal imu

--- src/robot-config/sensors.cpp
+++ src/robot-config/sensors.cpp
@@ -3,7 +3,7 @@
 #include "robot.h"
 
 pros::Rotation Robot::Sensors::vert {14, false};
-pros::Rotation Robot::Sensors::hori {7, false};
+pros::Rotation Robot::Sensors::hori {7, true};
 pros::Imu Robot::Sensors::imuA {3};
 pros::Imu Robot::Sensors::imuB {4};
 pros::Imu Robot::Sensors::imuC {16};
