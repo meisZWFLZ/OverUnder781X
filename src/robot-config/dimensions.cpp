0d286e3372b813f2ed98f05c09e0336e97bcce47
meisZWFLZ <atcurtis08@gmail.com>
2024-02-19 15:23:27 -0700

    fix: :wrench: tune pod offsets

--- src/robot-config/dimensions.cpp
+++ src/robot-config/dimensions.cpp
@@ -1,17 +1,16 @@
 #include "robot.h"
 #include "lemlib/chassis/trackingWheel.hpp"
 
-const float Robot::Dimensions::trackWidth = 11.25;
+const float Robot::Dimensions::trackWidth = 11.25 - 1.0/32;
 const float Robot::Dimensions::driveWheelDiameter = 3.25;
 const float Robot::Dimensions::driveWheelRpm = 360;
 const float Robot::Dimensions::driveEncGearRatio = 2;
 
 const float Robot::Dimensions::vertEncDiameter = lemlib::Omniwheel::NEW_275;
 const float Robot::Dimensions::vertEncDistance =
-    0 /* Robot::Dimensions::trackWidth / 2 */;
+    -1.0 / 8 /* Robot::Dimensions::trackWidth / 2 */;
 const float Robot::Dimensions::vertEncGearRatio = 1;
 
-const float Robot::Dimensions::horiEncDiameter =
-    lemlib::Omniwheel::NEW_275_HALF;
-const float Robot::Dimensions::horiEncDistance = -4.157;
+const float Robot::Dimensions::horiEncDiameter = lemlib::Omniwheel::NEW_275;
+const float Robot::Dimensions::horiEncDistance = -5.274;
 const float Robot::Dimensions::horiEncGearRatio = 1;
