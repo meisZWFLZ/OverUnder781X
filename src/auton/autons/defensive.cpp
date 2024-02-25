2fcecfa8d2874c9ca8253bbc7775e9c94dd874e5
meisZWFLZ <atcurtis08@gmail.com>
2024-02-25 15:32:39 -0700

    feat: :sparkles: working awp defensive auto

--- src/auton/autons/defensive.cpp
+++ src/auton/autons/defensive.cpp
@@ -12,35 +12,53 @@ using namespace auton::utils;
 
 void runDefensive() {
   Robot::chassis->setPose(
-      {0 - TILE_LENGTH * 2, MIN_Y + TILE_LENGTH - 7.125, UP}, false);
+      {MIN_X + TILE_LENGTH + Robot::Dimensions::drivetrainLength / 2,
+       MIN_Y + TILE_LENGTH - Robot::Dimensions::drivetrainWidth / 2, RIGHT},
+      false);
 
-  // Robot::chassis->turnTo(TILE_LENGTH * 2, 0, 1500, true, 40);
-  // pros::delay(20);
-  // Robot::Pistons::blocker.set_value(false);
-  // Robot::chassis->waitUntilDone();
-  // pros::delay(250);
+  // back up to get in better position to remove the matchload zone triball
+  Robot::chassis->moveToPoint(
+      MIN_X + TILE_LENGTH,
+      MIN_Y + TILE_LENGTH - Robot::Dimensions::drivetrainWidth / 2, 2000,
+      {
+          .forwards = false,
+          .maxSpeed = 72,
+      });
+  Robot::chassis->waitUntilDone();
 
-  // Robot::Subsystems::catapult->matchload(1000);
+  // turn to be parallel with matchload barrier
+  Robot::chassis->turnTo(1000000, -1000000, 2000, true, 48);
+  Robot::chassis->waitUntilDone();
+
+  // let wing expand
+  Robot::Actions::expandBackWing();
+  pros::delay(500);
+  
+  // remove the matchload zone triball
+  Robot::chassis->turnTo(1000000, 0, 2000);
+  Robot::chassis->waitUntilDone();
+  
+  // let ball roll away
   pros::delay(1000);
-  // Robot::Actions::prepareRobot();
 
-  Robot::Actions::expandBothWings();
-  tank(-127, 127, 1000);
-  Robot::Actions::retractBothWings();
+  // retract wing so it doesn't get in the way
+  Robot::Actions::retractBackWing();
+  
+  // intake any straggler triballs
+  Robot::Actions::intake();
 
-  // const lemlib::Pose targetPose {0 - 10, MIN_Y + TILE_RADIUS + 2, RIGHT};
+  // touch horizontal elevation bar
+  Robot::chassis->moveToPose(0 - Robot::Dimensions::drivetrainLength / 2 - 3.5,
+                             MIN_Y + TILE_RADIUS, RIGHT, 2000);
 
-  // Robot::chassis->turnTo(targetPose.x, targetPose.y, 1500);
-  // Robot::chassis->waitUntilDone();
-  // Robot::chassis->moveToPose(targetPose.x + 20, targetPose.y, targetPose.theta,
-  //                            3500, {.maxSpeed = 64});
-  // Robot::Actions::outtake();
-  // waitUntilDistToPose({targetPose.x + 3, targetPose.y}, 3, 0, true);
-  // Robot::chassis->cancelMotion();
-  // tank(32, 32, 100);
-  // stop();
-  // pros::delay(500);
-  // Robot::Actions::stopIntake();
+  // if a triball enters the intake, outtake it
+  while (pros::competition::is_autonomous()) {
+    if (isTriballInIntake()) {
+      Robot::Actions::outtake();
+      pros::delay(500);
+    }
+    pros::delay(10);
+  }
 }
 
 auton::Auton auton::autons::defensive = {(char*)("defensive / left"),
