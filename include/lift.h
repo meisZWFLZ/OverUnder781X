bec1904a6a1fb7559ddf5108084571b443f81076
meisZWFLZ <atcurtis08@gmail.com>
2024-03-08 00:24:15 -0700

    docs: :bulb: add comments to lift source code

--- include/lift.h
+++ include/lift.h
@@ -1,35 +1,35 @@
 #pragma once
 
-#include "lemlib/pid.hpp"
-#include "lemlib/timer.hpp"
-#include "pros/motors.h"
-#include "pros/motors.hpp"
 #include "pros/adi.hpp"
-#include <vector>
-
-struct PIDControllerSettings {
-    float kP;
-    float kI;
-    float kD;
-    float windupRange = 0;
-    bool signFlipReset = true;
-};
 
 class LiftArmStateMachine {
   public:
+    // our possible states
     enum STATE { IDLE, RETRACTING, EXTENDING };
 
+    // initializes state machine object and gives the machine the solenoids
     LiftArmStateMachine(pros::ADIDigitalOut* retract,
                         pros::ADIDigitalOut* extend);
 
+    // retrieve the current state of the state machine
     STATE getState() const;
+
+    // state changes
+    // set the state to retracting
     void retract();
+    // set the state to idle
     void release();
+    // set the state to extending
     void extend();
 
+    // run every 10 ms
     void update();
+
+    // these members cannot be accessed from outside the class
   private:
+    // the solenoids
     pros::ADIDigitalOut* retractPiston;
     pros::ADIDigitalOut* extendPiston;
+    // the current state
     STATE state = IDLE;
 };
--- src/subsystems/lift.cpp
+++ src/subsystems/lift.cpp
@@ -1,7 +1,6 @@
 #include "lift.h"
-#include "lemlib/util.hpp"
-#include <numeric>
 
+// construct the lift arm state machine
 LiftArmStateMachine::LiftArmStateMachine(pros::ADIDigitalOut* retract,
                                          pros::ADIDigitalOut* extend)
   : retractPiston(retract), extendPiston(extend) {
@@ -9,28 +8,42 @@ LiftArmStateMachine::LiftArmStateMachine(pros::ADIDigitalOut* retract,
   this->extendPiston->set_value(false);
 };
 
+// run every 10 ms
 void LiftArmStateMachine::update() {
   switch (this->state) {
-    // when in the idle
+    // when in the idle state:
     case IDLE:
+      // set both to false (ie: close the valves)
       this->retractPiston->set_value(false);
       this->extendPiston->set_value(false);
       break;
+
+    // when in the retracting state:
     case RETRACTING:
+      // only set retractPiston to true (ie: only open the retract valve)
       this->retractPiston->set_value(true);
       this->extendPiston->set_value(false);
       break;
+
+    // when in the extended state:
     case EXTENDING:
+      // only set extendPiston to true (ie: only open the expand valve)
       this->retractPiston->set_value(false);
       this->extendPiston->set_value(true);
       break;
   }
 }
 
+// extend the pistons
 void LiftArmStateMachine::extend() { this->state = EXTENDING; }
+
+// retract the pistons
 void LiftArmStateMachine::retract() { this->state = RETRACTING; }
+
+// stop sending air to the pistons
 void LiftArmStateMachine::release() { this->state = IDLE; }
 
+// get the current state of the lift arm state machine
 LiftArmStateMachine::STATE LiftArmStateMachine::getState() const {
   return this->state;
 }
