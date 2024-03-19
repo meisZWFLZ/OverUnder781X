6f678847694de62131e860e48d302b63a9c52c6d
meisZWFLZ <atcurtis08@gmail.com>
2024-03-19 12:54:15 -0700

    fix: :zap: improve skills

--- src/auton/autons/skills.cpp
+++ src/auton/autons/skills.cpp
@@ -141,7 +141,7 @@ void runSkills() {
   Robot::Actions::retractBothWings();
   const lemlib::Pose outOfTheGoalTarget1 {
       0 + Robot::Dimensions::drivetrainWidth / 2 + 2,
-      Robot::chassis->getPose().y - 12, UP};
+      Robot::chassis->getPose().y - 9, UP};
   Robot::chassis->moveToPose(outOfTheGoalTarget1.x, outOfTheGoalTarget1.y,
                              outOfTheGoalTarget1.theta, 3000,
                              {.forwards = false, .minSpeed = 64});
@@ -183,7 +183,7 @@ void runSkills() {
 
   // get out of goal
   lemlib::Pose outOfTheGoalTarget2 {Robot::Dimensions::drivetrainLength / 2 + 2,
-                                    TILE_LENGTH};
+                                    TILE_LENGTH - 3};
   Robot::chassis->moveToPoint(outOfTheGoalTarget2.x, outOfTheGoalTarget2.y,
                               1000, {.forwards = false, .minSpeed = 64});
   // wait near pose
@@ -197,10 +197,10 @@ void runSkills() {
 
   // sweep triballs in barrier corner
   lemlib::Pose barrierCornerSweepTarget {
-      TILE_LENGTH * 1.8,
-      TILE_LENGTH * 2 - Robot::Dimensions::drivetrainWidth / 2 - 1};
+      TILE_LENGTH * 1.9,
+      TILE_LENGTH * 2 - Robot::Dimensions::drivetrainWidth / 2 - 2};
   Robot::chassis->moveToPose(barrierCornerSweepTarget.x,
-                             barrierCornerSweepTarget.y, LEFT, 2000,
+                             barrierCornerSweepTarget.y, LEFT - 15, 1500,
                              {.forwards = false, .minSpeed = 64});
   Robot::Actions::expandBackWing();
   // wait until near pose
@@ -242,7 +242,7 @@ void runSkills() {
 
   // wait until near goal to ram at full power
   waitUntil([] {
-    return Robot::chassis->getPose().x < TILE_LENGTH * 1.5 ||
+    return Robot::chassis->getPose().y < TILE_LENGTH * 1.5 ||
            !isMotionRunning();
   });
   Robot::chassis->cancelMotion();
@@ -254,9 +254,9 @@ void runSkills() {
 
   // second ram
   // back out of goal
-  tank(127, 127, 300, 3);
+  tank(127, 127, 400, 3);
   // ram into goal
-  Robot::chassis->moveToPoint(0, -10000000, 700,
+  Robot::chassis->moveToPoint(0, -10000000, 800,
                               {.forwards = false, .minSpeed = 127});
   Robot::chassis->waitUntilDone();
   // back out of goal
