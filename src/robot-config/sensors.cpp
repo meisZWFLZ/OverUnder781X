0bd3964e35d42ac793c41893a3d6c510225f1a86
meisZWFLZ <atcurtis08@gmail.com>
2024-03-22 15:39:21 -0700

    fix: :alien: change catapult ports

--- src/robot-config/sensors.cpp
+++ src/robot-config/sensors.cpp
@@ -8,4 +8,4 @@ pros::Imu Robot::Sensors::imuA {3};
 pros::Imu Robot::Sensors::imuB {4};
 pros::Imu Robot::Sensors::imuC {16};
 pros::ADILineSensor Robot::Sensors::cataElevationBar {'B'};
-pros::Rotation Robot::Sensors::cata {6, true};
+pros::Rotation Robot::Sensors::cata {5, true};
